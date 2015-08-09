8cacddd7e396c3e11fea9844a5e2738b011e73da
macbel <mblazquezm@gmail.com>
2015-08-09 14:06:02 +0200

    Edición de preguntas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 //var partials = require('express-dynamic-partials');
 var routes = require('./routes/index');
 
@@ -21,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(methodOverride('_method'));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
